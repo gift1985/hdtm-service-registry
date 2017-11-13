57f3f1bb975423ba8103d9fd184b63f78f102a82
gift1985 <gift1985@163.com>
2017-11-13 19:27:08 +0800

    加入验证，增强健壮性

---
+++ src/test/java/com/holdtime/microservices/registry/HdtmServiceRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.holdtime.microservices.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HdtmServiceRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
